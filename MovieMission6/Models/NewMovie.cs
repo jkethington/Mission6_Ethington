3a7f86746a41627eca499a586dc578dd4380c509
jkethington <jkethington@gmail.com>
2026-02-23 14:39:26 -0700

    nullables

--- MovieMission6/Models/NewMovie.cs
+++ MovieMission6/Models/NewMovie.cs
@@ -6,26 +6,26 @@ namespace MovieMission6.Models
     public class NewMovie
     {
         [Key]
-        public int MovieId { get; set; }   // Primary Key
+        public int? MovieId { get; set; }   // Primary Key
 
-        public int CategoryId { get; set; }   // FK
+        public int? CategoryId { get; set; }   // FK
 
         [ForeignKey("CategoryId")]
         public Category? Category { get; set; }   // navigation property
 
 
         [Required(ErrorMessage="Please enter a movie title")]
-        public string? Title { get; set; } = "";
+        public string Title { get; set; } = "";
 
         [Required]
         [Range(1888, 2100, ErrorMessage = "Year must be 1888 or later.")]
         public int? Year { get; set; }
 
         
-        public string Director { get; set; } = "";
+        public string? Director { get; set; } = "";
 
         
-        public string Rating { get; set; } = "";
+        public string? Rating { get; set; } = "";
 
         [Required]
         public bool? Edited { get; set; }
